fdb6b1fa1bd31d3bb94be2bb212cb51e8f97d5cc
Baizhou Chen <b99chen@edu.uwaterloo.ca>
2021-03-20 19:37:34 -0400

    adding style for comment section, minor change in index.js, minor restructure scss file into common.scss
    Signed-off-by: Baizhou Chen <b99chen@edu.uwaterloo.ca>

--- sprint-2/scripts/index.js
+++ sprint-2/scripts/index.js
@@ -1,8 +1,9 @@
 // create Comment object model using constructor function
-function Comment(name, date, description) {
+function Comment(name, date, description, img) {
   this.name = name;
   this.date = date;
   this.description = description;
+  this.img = img;
 }
 
 // returns a formated date
@@ -16,13 +17,9 @@ function getDate() {
 
 // create a empty element and returns a function that aquire a variable to fill the content
 function newElement(tag, classNames) {
-  let element = document.createElement(tag);
-
-  for (let i = 0; i < classNames.length; ++i) {
-    element.classList.add(classNames[i]);
-  }
-
   return (content = "") => {
+    let element = document.createElement(tag);
+    element.classList.add(...classNames);
     if (content) element.innerText = content;
     return element;
   };
@@ -30,22 +27,25 @@ function newElement(tag, classNames) {
 
 // append children element (array of child elements) into parent element
 function insertBlock(parent, children) {
-  console.log[children[0]];
   for (let i = 0; i < children.length; ++i) {
     parent.appendChild(children[i]);
   }
 }
 
+// make reusable functions for creat
+const liTag = newElement("li", ["comment__item"]);
+const imgTag = newElement("img", [
+  "comment__item--left",
+  "comment__profile-pic",
+]);
+const contentTag = newElement("div", ["comment__item--right"]);
+const titleTag = newElement("div", ["comment__item--title"]);
+const nameTag = newElement("h4", ["comment__item--name"]);
+const dateTag = newElement("h4", ["comment__item--date"]);
+const descriptionTag = newElement("p", ["comment__item--description"]);
+
 // aquire a new Comment object and insert into document
 function newComment(comment) {
-  const liTag = newElement("li", ["comment__item"]);
-  const imgTag = newElement("img", ["comment__item--left"]);
-  const contentTag = newElement("div", ["comment__item--right"]);
-  const titleTag = newElement("div", ["comment__item--title"]);
-  const nameTag = newElement("h4", ["comment__item--name"]);
-  const dateTag = newElement("h4", ["comment__item--date"]);
-  const descriptionTag = newElement("p", ["comment__item--description"]);
-
   let nameBlock = nameTag(comment.name);
   let dateBlock = dateTag(comment.date);
   let titleBlock = titleTag();
@@ -56,6 +56,7 @@ function newComment(comment) {
   insertBlock(contentBlock, [titleBlock, descriptionBlock]);
 
   let imgBlock = imgTag();
+  imgBlock.src = comment.img;
   let liBlock = liTag();
   insertBlock(liBlock, [imgBlock, contentBlock]);
 
@@ -66,8 +67,8 @@ function newComment(comment) {
 //////////////// event handling ////////////////
 // global variables
 const form = document.querySelector(".comment__form");
-const nameField = document.querySelector(".comment__input--name");
-const descriptionField = document.querySelector(".comment__input--description");
+const nameField = document.querySelector(".comment__form--name");
+const descriptionField = document.querySelector(".comment__form--description");
 
 // comment onSubmit event handling, create a new Comment object and
 function handleOnSubmit(e) {
@@ -75,9 +76,12 @@ function handleOnSubmit(e) {
   const name = e.target.name.value;
   const description = e.target.description.value;
   const date = getDate();
-  const comment = new Comment(name, date, description);
+  console.log(typeof description);
+  const img = "./assets/Images/Mohan-muruge.jpg";
+  const comment = new Comment(name, date, description, img);
   newComment(comment);
 }
+
 // add event listener on form, will be trigered by clicking submit button
 form.addEventListener("submit", handleOnSubmit);
 
@@ -86,24 +90,30 @@ form.addEventListener("submit", handleOnSubmit);
 function addMockdata() {
   // comment one
   const name1 = "Theodore Duncan";
-  const date1 = getDate();
+  const date1 = "11/15/2018";
   const description1 =
-    "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.";
-  const comment1 = new Comment(name1, date1, description1);
+    "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!";
+  const img1 =
+    "https://emoji.slack-edge.com/T01N11LCR55/surprised-jon/016aed46b077ef79.png";
+  const comment1 = new Comment(name1, date1, description1, img1);
 
   // comment two
   const name2 = "Gary Wong";
-  const date2 = getDate();
+  const date2 = "12/12/2018";
   const description2 =
     "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!";
-  const comment2 = new Comment(name2, date2, description2);
+  const img2 =
+    "https://emoji.slack-edge.com/T01N11LCR55/nuge-thumbs-up/af2d083ed763c373.jpg";
+  const comment2 = new Comment(name2, date2, description2, img2);
 
   // comment three
   const name3 = "Micheal Lyons";
-  const date3 = getDate();
+  const date3 = "12/18/2018";
   const description3 =
     "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.";
-  const comment3 = new Comment(name3, date3, description3);
+  const img3 =
+    "https://emoji.slack-edge.com/T01N11LCR55/nuge-amazed/27e213d16cb2560d.jpg";
+  const comment3 = new Comment(name3, date3, description3, img3);
 
   // create array for stroing comment objects
   const comments = [comment1, comment2, comment3];
