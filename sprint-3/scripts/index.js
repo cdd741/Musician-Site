30710aa031b4a59e0607be02d97b5893ef113cb4
Baizhou Chen <b99chen@edu.uwaterloo.ca>
2021-03-24 01:18:01 -0400

    comment section function, styling and diving deeper

--- sprint-3/scripts/index.js
+++ sprint-3/scripts/index.js
@@ -1,13 +1,5 @@
-// create an array for stroing comment objects
-const comments = [];
-
-// create Comment object model using constructor function
-function Comment(name, date, description, img) {
-  this.name = name;
-  this.date = date;
-  this.description = description;
-  this.img = img;
-}
+let deleteBtns;
+let likeBtns;
 
 function Unit(name, time) {
   this.name = name;
@@ -21,12 +13,12 @@ const day = new Unit("day", 86400);
 const hour = new Unit("hour", 3600);
 const minut = new Unit("minut", 60);
 const second = new Unit("second", 1);
-
 const timeUnits = [year, month, week, day, hour, minut, second];
 
 function getTimePassed(date) {
   let timeNow = new Date().getTime();
-  let timeThen = date.getTime();
+  // let timeThen = date.getTime();
+  let timeThen = date;
   let timeDiff = timeNow - timeThen;
   timeDiff = Math.round(timeDiff / 1000);
 
@@ -60,109 +52,134 @@ const titleTag = newElement("div", ["comment__item--title"]);
 const nameTag = newElement("h4", ["comment__item--name"]);
 const dateTag = newElement("h4", ["comment__item--date"]);
 const descriptionTag = newElement("p", ["comment__item--description"]);
+const deleteTag = newElement("button", ["comment__item--delete-btn"]);
+const likeTag = newElement("button", ["comment__item--like-btn"]);
+const lowerTag = newElement("div", ["comment__item--lowerWrap"]);
 
 const commentList = document.querySelector(".comment__list");
 
 // aquire a new Comment object and insert into document
 function displayComment(comment) {
   const nameBlock = nameTag(comment.name);
-  const timePassed = getTimePassed(comment.date);
+  const timePassed = getTimePassed(comment.timestamp);
   const dateBlock = dateTag(timePassed);
   const titleBlock = titleTag();
-  insertBlock(titleBlock, [nameBlock, dateBlock]);
-
-  const descriptionBlock = descriptionTag(comment.description);
+  const descriptionBlock = descriptionTag(comment.comment);
   const contentBlock = contentTag();
-  insertBlock(contentBlock, [titleBlock, descriptionBlock]);
-
+  const deletBtn = deleteTag("delete");
+  const likeBtn = likeTag(`${comment.likes} ❤`);
+  if (comment.likes > 0) likeBtn.classList.add("comment__item--liked");
+  const lowerWrap = lowerTag();
   const imgBlock = imgTag();
-  imgBlock.src = comment.img;
   const liBlock = liTag();
+  liBlock.id = comment.id;
+  insertBlock(titleBlock, [nameBlock, dateBlock]);
+  insertBlock(lowerWrap, [deletBtn, likeBtn]);
+  insertBlock(contentBlock, [titleBlock, descriptionBlock, lowerWrap]);
   insertBlock(liBlock, [imgBlock, contentBlock]);
-
   insertBlock(commentList, [liBlock]);
+
+  deleteBtns = document.querySelectorAll(".comment__item--delete-btn");
+  likeBtns = document.querySelectorAll(".comment__item--like-btn");
+  for (let i = 0; i < deleteBtns.length; ++i) {
+    deleteBtns[i].addEventListener("click", handleDelete);
+    likeBtns[i].addEventListener("click", handleLike);
+  }
 }
 
-function newCommentList(commentList) {
-  commentList.forEach((comment) => {
+function newCommentList(comments) {
+  console.log("comment refreshed");
+  commentList.innerHTML = "";
+  comments.forEach((comment) => {
+    console.log(comment.likes);
     displayComment(comment);
   });
 }
 
+// Api calls
+// GET
+function getComments() {
+  axios
+    .get(`https://project-1-api.herokuapp.com/comments?api_key=<${apiKey}>`)
+    .then((res) => {
+      newCommentList(res.data);
+    })
+    .catch((err) => console.log(err));
+}
+
+// POST
+function postComments(name, comment) {
+  axios
+    .post(`https://project-1-api.herokuapp.com/comments?api_key=<${apiKey}>`, {
+      name: name,
+      comment: comment,
+    })
+    .then(() => {
+      getComments();
+    })
+    .catch((err) => console.log(err));
+}
+
+// PUT
+function likeComment(id) {
+  axios
+    .put(
+      `https://project-1-api.herokuapp.com/comments/${id}/like?api_key=<${apiKey}>`
+    )
+    .then(() => {
+      getComments();
+    })
+    .catch((err) => console.log(err));
+}
+
+//DELETE
+function deleteComment(id) {
+  axios
+    .delete(
+      `https://project-1-api.herokuapp.com/comments/${id}?api_key=<${apiKey}>`
+    )
+    .then(() => {
+      getComments();
+    })
+    .catch((err) => console.log(err));
+}
+
+const apiKey = "5c7904ce-9fd9-4d9a-91be-11685db1d644";
+getComments();
+
 //////////////// event handling ////////////////
 // global variables
 const form = document.querySelector(".comment__form");
-const nameField = document.querySelector(".comment__form--name");
-const descriptionField = document.querySelector(".comment__form--description");
+const inputName = document.querySelector(".comment__form--name");
+const inputDescripstion = document.querySelector(".comment__form--description");
 
 // comment onSubmit event handling, create a new Comment object and
 function handleOnSubmit(e) {
   e.preventDefault();
-
   // get input info and date for comment
   const name = e.target.name.value;
   const description = e.target.description.value;
-  const date = getDate();
-  const img = "./assets/Images/Mohan-muruge.jpg";
-
   if (!name || !description) {
     alert("Please Fill All Required Field");
     return;
   }
-
   // reset input field
-  const inputName = document.querySelector(".comment__form--name");
-  const inputDescripstion = document.querySelector(
-    ".comment__form--description"
-  );
-
   inputName.value = "";
   inputDescripstion.value = "";
+  postComments(name, description);
+}
 
-  // construct comment object and create new comment block
-  const comment = new Comment(name, date, description, img);
-  comments.push(comment);
-  commentList.innerHTML = "";
-  newCommentList(comments);
+function handleLike(e) {
+  e.preventDefault();
+  let id = e.target.parentNode.parentNode.parentNode.id;
+  likeComment(id);
+}
+
+function handleDelete(e) {
+  e.preventDefault();
+  let id = e.target.parentNode.parentNode.parentNode.id;
+  deleteComment(id);
 }
 
 // add event listener on form, will be trigered by clicking submit button
 form.addEventListener("submit", handleOnSubmit);
-
-// mocking
-// create mockup data for comments
-function addMockdata() {
-  // comment one
-  const name1 = "Theodore Duncan";
-  const date1 = new Date("Nov 15 2018");
-  const description1 =
-    "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!";
-  const img1 =
-    "https://emoji.slack-edge.com/T01N11LCR55/surprised-jon/016aed46b077ef79.png";
-  const comment1 = new Comment(name1, date1, description1, img1);
-
-  // comment two
-  const name2 = "Gary Wong";
-  const date2 = new Date("Dec 12 2018");
-  const description2 =
-    "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!";
-  const img2 =
-    "https://emoji.slack-edge.com/T01N11LCR55/nuge-thumbs-up/af2d083ed763c373.jpg";
-  const comment2 = new Comment(name2, date2, description2, img2);
-
-  // comment three
-  const name3 = "Micheal Lyons";
-  const date3 = new Date("Dec 18 2018");
-  const description3 =
-    "They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.";
-  const img3 =
-    "https://emoji.slack-edge.com/T01N11LCR55/nuge-amazed/27e213d16cb2560d.jpg";
-  const comment3 = new Comment(name3, date3, description3, img3);
-
-  // create array for stroing comment objects
-  comments.push(comment1, comment2, comment3);
-  newCommentList(comments);
-}
-
-// execut mocking function
-addMockdata();
