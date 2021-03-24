d60bdab63602e6782fb90eee1fb86f25b95af864
Baizhou Chen <b99chen@edu.uwaterloo.ca>
2021-03-24 01:38:09 -0400

    adding RestAPIs to get info from backend

--- sprint-3/scripts/shows.js
+++ sprint-3/scripts/shows.js
@@ -23,7 +23,7 @@ function newShow(show) {
 
   const venueLabel = labelTag("VENUE");
   venueLabel.classList.add("show__label--venue");
-  const venueBlock = venueTag(show.venue);
+  const venueBlock = venueTag(show.place);
   const venueWrapper = wrapper();
   insertBlock(venueWrapper, [venueLabel, venueBlock]);
 
@@ -40,42 +40,11 @@ function newShow(show) {
   insertBlock(showList, [showBlock]);
 }
 
-function mockupData() {
-  const date1 = "Mon Dec 17 2018";
-  const date2 = "Tue Jul 18 2019";
-  const date3 = "Fri Jul 22 2019";
-  const date4 = "Sat Aug 12 2019";
-  const date5 = "Fri Sep 05 2019";
-  const date6 = "Wed Aug 11 2019";
-  const dates = [date1, date2, date3, date4, date5, date6];
-
-  const venue1 = "Ronald Lane";
-  const venue2 = "Pier 3 East";
-  const venue3 = "View Loungue";
-  const venue4 = "Hyatt Agency";
-  const venue5 = "Moscow Center";
-  const venue6 = "Pres Club";
-  const venues = [venue1, venue2, venue3, venue4, venue5, venue6];
-
-  const location1 = "San Fancisco, CA";
-  const location2 = "San Fancisco, CA";
-  const location3 = "San Fancisco, CA";
-  const location4 = "San Fancisco, CA";
-  const location5 = "San Fancisco, CA";
-  const location6 = "San Fancisco, CA";
-  const locations = [
-    location1,
-    location2,
-    location3,
-    location4,
-    location5,
-    location6,
-  ];
-
-  for (let i = 0; i < dates.length; ++i) {
-    const show = new Show(dates[i], venues[i], locations[i]);
-    newShow(show);
+function displayShows(shows) {
+  for (let i = 0; i < shows.length; ++i) {
+    newShow(shows[i]);
   }
 }
 
-mockupData();
+// call api functions from apis.js
+getShows();
